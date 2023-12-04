5a768fc7e4323fe74db6421eed87a3c5c642a8e4
d-baez <63023353+d-baez@users.noreply.github.com>
2023-12-03 20:05:34 -0500

    contour created

--- contour.py
+++ contour.py
@@ -6,13 +6,16 @@ cv.imshow('img', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', gray)
 
+blur = cv.GaussianBlur(gray,(5,5), cv.BORDER_DEFAULT)
+cv.imshow('blur',blur)
+
 #canny
-canny = cv.Canny(img, 125, 175)
+canny = cv.Canny(blur, 125, 175)
 cv.imshow('canny edge', canny)
 
 #contours
 countours, hiearchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
+print(f'{len(countours)} countors found')
 
 
 
